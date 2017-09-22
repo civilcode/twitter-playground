235b074e581d4782418b67f47a8bc0c57037f217
Hugo Frappier <hugo@cekoya.com>
2017-09-22 14:48:59 -0400

    Extract timeline javascript code into its own file

---
+++ apps/web/web/static/js/twitter_timeline.js
@@ -0,0 +1,38 @@
+let TwitterTimeline = {
+  init(socket, tweets_container) {
+    // Now that you are connected, you can join channels with a topic:
+    this.channel = socket.channel("tweets", {})
+    this.container = $(tweets_container)
+    this.tweets = $(tweets_container).find(".tweets")
+
+    this.channel.join()
+      .receive("ok", resp => { console.log("Joined successfully", resp) })
+      .receive("error", resp => { console.log("Unable to join", resp) })
+
+    this.channel.onError(e => console.log("something went wrong", e))
+    this.channel.onClose(e => console.log("channel closed", e))
+
+    this.channel.on("refresh_list", msg => {
+      this._refreshAll(msg.tweets)
+    })
+
+    this.channel.on("new_tweet", tweet => {
+      this._appendTweet(tweet)
+    })
+  },
+
+  _refreshAll(tweets) {
+    this.tweets.empty()
+    for(var tweet of tweets) {
+      this.tweets.prepend($('<li>').text(tweet.text))
+  }
+    this.container.scrollTop(this.container[0].scrollHeight);
+  },
+
+  _appendTweet(tweet) {
+    this.tweets.ppend($('<li>').text(twet.text))
+    this.container.scrollTop(this.container[0].scrollHeight);
+  }
+}
+
+export default TwitterTimeline
